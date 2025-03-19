80381d0c2ed5b78b3686956c86d31f5e45c71dda
Mannan <Mannan@DESKTOP-OTIQOF5>
2025-03-19 22:26:31 +0530

    refactor: Update AuthorData and ChiefAuthorData interfaces

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,10 @@
 export interface AuthorData {
+  designation: any;
   image: string;
   name: string;
 }
 
 export interface ChiefAuthorData extends AuthorData {
+  designation: any;
   title: string;
 }
